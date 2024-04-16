0ba364c913de1d6919f1f787034172360c790712
Sohaib Khan <sohaibk13@gmail.com>
2024-04-16 12:24:09 +0530

    adding DDR for two reports

--- ddrweb/urls.py
+++ ddrweb/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ddrweb, name='ddrweb-home'),
+    path('viewReport/<slug:fileName>/', views.viewReport, name='ddrweb-viewReport'),
 ]
--- ddrweb/views.py
+++ ddrweb/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pandas as pd
+import os
+from django.conf import settings
 
 def ddrweb(request):
-    return render(request, 'ddrweb/ddrweb.html')
+    directory_path = os.path.join(settings.BASE_DIR , 'reports')
+    
+    reports = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
+
+    return render(request, 'ddrweb/ddrweb.html', {'reports':reports})
+
+def viewReport(request, fileName):
+    filename = '../reports/'+str(fileName)
+    df = pd.read_excel(filename, header=2, index_col=0)
+
+    try:
+        value = df.at['Total', 'Amount']
+        return render(request, 'ddrweb/viewReport.html', df)
+    except KeyError as e:
+        print(f"Key error: {e} - Check if the specified row or column exists.")
