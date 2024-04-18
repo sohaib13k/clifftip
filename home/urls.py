1d99b22c3ec594780f8254b440c22ae0ff86b0c3
sohaib khan <sohaibk13@gmail.com>
2024-04-18 15:50:45 +0530

    - integrating login page

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home-index'),
-    # path("", include("account.urls")),
+    path("home/", views.home, name="home-home"),
+    path("", include("account.urls")),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, "home/index.html")
+def home(request):
+    return render(request, "home/home.html")
