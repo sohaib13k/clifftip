7f158c491915a69b88a610ca0267bf60bfae43bb
sohaib khan <sohaibk13@gmail.com>
2024-04-18 11:25:00 +0530

    - adding setttings.SECRET_KEY in .env

--- SukhAnalytics/settings.py
+++ SukhAnalytics/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-d^b)lbv@2-!npiuh(-)y5+tru#n$=_)7pd(qv3dsx_e$d)4wu3"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "True" == os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ["localhost", "sohaib13k.pythonanywhere.com"]
+
 
 # Application definition
 
