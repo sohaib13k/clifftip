c75b5b4df53b43cc58edfffd4bfb22c7d6f28576
sohaib khan <sohaibk13@gmail.com>
2024-04-19 11:26:09 +0530

    fixing .env file loading issue

--- manage.py
+++ manage.py
@@ -2,22 +2,23 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.conf import settings
+from pathlib import Path
 from dotenv import load_dotenv
 
 
 def load_env_file():
-    directory_path = settings.BASE_DIR / ".env"
+    BASE_DIR = Path(__file__).resolve().parent
+    directory_path = BASE_DIR / ".env"
     if not directory_path.exists():
-        raise FileNotFoundError("Cannot find .env file at BASE_DIR")
+        raise FileNotFoundError("Cannot find .env file at", BASE_DIR)
     load_dotenv()
 
 
 def main():
     """Run administrative tasks."""
+    load_env_file()  # loading environment variables from .env
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SukhAnalytics.settings")
     try:
-        load_env_file()
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
