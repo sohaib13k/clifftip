b4feb52bb2b776eb8868437276da5b5a5a555b70
Sohaib Khan <sohaibk13@gmail.com>
2024-05-10 20:43:28 +0530

    - customising admin panel

--- report/admin.py
+++ report/admin.py
@@ -15,20 +15,93 @@ class ReportAdmin(admin.ModelAdmin):
 
     search_fields = ["name"]
 
-    list_filter = ['access_users', 'access_groups']
+    list_filter = ["access_users", "access_groups"]
 
     readonly_fields = ("created_by", "report_last_updated_tmstmp")
 
     exclude = ["is_datetime_merged"]
+
+    fieldsets = [
+        (
+            "General Information",
+            {
+                "fields": (
+                    "name",
+                    "data_source",
+                    "is_masterdata",
+                    "date_col",
+                    "time_col",
+                ),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": ("access_users", "access_groups"),
+                "classes": ("collapse",),  # Optional: Collapse the fieldset by default
+            },
+        ),
+        (
+            "System Information",
+            {
+                "fields": ("report_last_updated_tmstmp", "created_by"),
+                "classes": ("collapse",),  # Optional: Collapse the fieldset by default
+            },
+        ),
+        (
+            "Custom Reports",
+            {
+                "fields": (
+                    "is_custom_report",
+                    "reports",
+                ),
+                "classes": ("collapse",),  # Optional: Collapse the fieldset by default
+            },
+        ),
+    ]
 
     def view_on_site(self, obj):
         pass
 
 
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'department', 'position']
-    search_fields = ['first_name', 'last_name', 'department', 'position', 'phone_number']
-    list_filter = ['department', 'position']
+    list_display = ["first_name", "last_name", "department", "position"]
+
+    search_fields = [
+        "first_name",
+        "last_name",
+        "department",
+        "position",
+        "phone_number",
+    ]
+
+    list_filter = ["department", "position"]
+
+    fieldsets = [
+        (
+            "Personal Information",
+            {
+                "fields": [
+                    "first_name",
+                    "last_name",
+                    "phone_number",
+                ],
+            },
+        ),
+        (
+            "Employment Details",
+            {
+                "fields": ("employee_company_id", "position", "department"),
+            },
+        ),
+        (
+            "Hierarchy",
+            {
+                "fields": ("manager_id", "supervisor_id"),
+                "classes": ("collapse",),  # Optional: Collapse the fieldset by default
+            },
+        ),
+    ]
 
 
 admin.site.register(Report, ReportAdmin)
