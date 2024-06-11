40859bfb25f4eff13866d92743d417a90fe68958
Sohaib Khan <sohaibk13@gmail.com>
2024-06-11 15:42:06 +0530

    Added field, which shows time elapsed since last report uploaded

---
+++ commonutil/templatetags/custom_filters.py
@@ -0,0 +1,23 @@
+from django import template
+from django.utils.timezone import now
+import datetime
+
+register = template.Library()
+
+@register.filter
+def time_elapsed(value):
+    if not value:
+        return ""
+    
+    delta = now() - value
+
+    if delta.days >= 1:
+        return f"{delta.days} days ago"
+    elif delta.seconds >= 3600:
+        hours = delta.seconds // 3600
+        return f"{hours} hours ago"
+    elif delta.seconds >= 60:
+        minutes = delta.seconds // 60
+        return f"{minutes} minutes ago"
+    else:
+        return "just now"
