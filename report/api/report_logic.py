5fedf250117e85fb20f3e58e59401e29f5c93f58
Sohaib Khan <sohaibk13@gmail.com>
2024-06-22 14:59:34 +0530

    Adding below tables-
    item type wise
    Branch wise
    Sales Person wise
    Item type by Branch wise
    Sales person by item type wise

--- report/api/report_logic.py
+++ report/api/report_logic.py
@@ -3,20 +3,91 @@ from report.commonutil import append_total, add_percentage_column
 
 
 def sale_purchase(request, report, filtered_data):
+    # Aggregating sales values
+    individual_sales = (
+        filtered_data.groupby("Sales Person").agg({"Net Total": "sum"}).reset_index()
+    )
+    individual_sales = append_total(individual_sales, "Sales Person", "Net Total")
+    individual_sales = add_percentage_column(individual_sales, "Net Total")
+
+    branch_sales = (
+        filtered_data.groupby("Branch").agg({"Net Total": "sum"}).reset_index()
+    )
+    branch_sales = append_total(branch_sales, "Branch", "Net Total")
+    branch_sales = add_percentage_column(branch_sales, "Net Total")
+
     item_type_sales = (
         filtered_data.groupby("Item Type").agg({"Net Total": "sum"}).reset_index()
     )
     item_type_sales = append_total(item_type_sales, "Item Type", "Net Total")
     item_type_sales = add_percentage_column(item_type_sales, "Net Total")
 
+    # import locale
+    # locale.setlocale(locale.LC_ALL, 'en_IN.utf8')
+    #
+    # def format_currency(value):
+    #     return locale.format_string("%d", value, grouping=True)
+
+    # item_type_sales = append_total(item_type_sales, "Item Type", "Net Total")
+    # item_type_sales['Net Total'] = item_type_sales['Net Total'].apply(format_currency)
+
+    individual_by_item_type_sales = (
+        filtered_data.groupby(["Sales Person", "Item Type"])
+        .agg({"Net Total": "sum"})
+        .unstack(fill_value=0)
+    )
+
+    branch_by_item_type_sales = (
+        filtered_data.groupby(["Branch", "Item Type"])
+        .agg({"Net Total": "sum"})
+        .unstack(fill_value=0)
+    )
+
+    # formatting numbers as per locale
+    # format_currency
+    individual_sales["Net Total"] = individual_sales["Net Total"].apply(
+        commonutil.format_rupees
+    )
+    branch_sales["Net Total"] = branch_sales["Net Total"].apply(
+        commonutil.format_rupees
+    )
     item_type_sales["Net Total"] = item_type_sales["Net Total"].apply(
         commonutil.format_rupees
     )
 
+    for column in individual_by_item_type_sales.columns:  # Skip the first column
+        individual_by_item_type_sales[column] = individual_by_item_type_sales[
+            column
+        ].apply(commonutil.format_rupees)
+
+    for column in branch_by_item_type_sales.columns:  # Skip the first column
+        branch_by_item_type_sales[column] = branch_by_item_type_sales[column].apply(
+            commonutil.format_rupees
+        )
+
+    # Convert to HTML for displaying in the template
+    sales_analysis = individual_sales.to_html(
+        classes="table table-striped", index=False, header=True
+    )
+    branch_analysis = branch_sales.to_html(
+        classes="table table-striped", index=False, header=True
+    )
     item_type_analysis = item_type_sales.to_html(
-         index=False, header=True
+        classes="table table-striped", index=False, header=True
+    )
+    individual_by_item_type_analysis = individual_by_item_type_sales.to_html(
+        classes="table table-striped", index=True, header=True
+    )
+    branch_by_item_type_analysis = branch_by_item_type_sales.to_html(
+        classes="table table-striped", index=True, header=True
     )
 
-    result = {"item_type_analysis": item_type_analysis}
+    result = {
+        "sales_analysis": sales_analysis,
+        "branch_analysis": branch_analysis,
+        "item_type_analysis": item_type_analysis,
+        "individual_by_item_type_analysis": individual_by_item_type_analysis,
+        "branch_by_item_type_analysis": branch_by_item_type_analysis,
+    }
 
     return result
