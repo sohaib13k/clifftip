5407767098d388d0e42386ec9bac07703a20b641
Sohaib Khan <sohaibk13@gmail.com>
2024-06-29 02:37:56 +0530

    Added caching for new-parties-addition-chart(dashboard). 90% load timing reduced

--- report/api/report_logic.py
+++ report/api/report_logic.py
@@ -1,3 +1,6 @@
+from django.utils import timezone
+from django.core.cache import cache
+from report.models import Report
 import pandas as pd
 from django.http import JsonResponse
 import os
@@ -109,45 +112,61 @@ def sale_purchase(request, report, filtered_data):
 
 def all_parties_with_sale(request, report, parties_with_sale):
     sale_reg_csv_dir = settings.CSV_DIR / "sale_register"
+    sale_register_model = Report.objects.get(service_name='sale_register')
+    all_parties_model = Report.objects.get(service_name='all_parties') # TODO: replace this with all_parties_with_sale
+    cache_key = 'all_parties_with_sale_cache222'
+    cache_tmstmp_key = 'all_parties_with_sale_cache_tmstmpqwqw'
+    cache_exist = False
 
-    try:
-        df_sales = pd.DataFrame()
-        for filename in os.listdir(sale_reg_csv_dir):
-            if filename.endswith(".csv"):
-                file_path = os.path.join(sale_reg_csv_dir, filename)
-                df = pd.read_csv(file_path)
-                df_sales = pd.concat([df_sales, df], ignore_index=True)
+    # Check if the result is already cached and if the cache is up-to-date
+    cache_tmstmp = cache.get(cache_tmstmp_key)
+    if cache_tmstmp and cache_tmstmp > max(sale_register_model.report_last_updated_tmstmp, all_parties_model.report_last_updated_tmstmp):
+        result = cache.get(cache_key)
+        if result is not None:
+            cache_exist = True
 
-    except FileNotFoundError:
-        return JsonResponse(
-            {"error": "CSV file not found"},
-            status=406,
-        )
+    if not cache_exist:
+        try:
+            df_sales = pd.DataFrame()
+            for filename in os.listdir(sale_reg_csv_dir):
+                if filename.endswith(".csv"):
+                    file_path = os.path.join(sale_reg_csv_dir, filename)
+                    df = pd.read_csv(file_path)
+                    df_sales = pd.concat([df_sales, df], ignore_index=True)
 
-    df_sales["Invoice Date"] = pd.to_datetime(df_sales["Invoice Date"])
+            df_sales["Invoice Date"] = pd.to_datetime(df_sales["Invoice Date"])
 
-    result = []
+            result = []
+            for index, row in parties_with_sale.iterrows():
+                customer_name = row["Company Name"]
+                branch = row["Branch.1"]
+                sales_person = row["Sales Person"]
+                gst_no = row["GST No."]
 
-    for index, row in parties_with_sale.iterrows():
-        customer_name = row["Company Name"]
-        branch = row["Branch.1"]
-        sales_person = row["Sales Person"]
-        gst_no = row["GST No."]
+                sales_data = df_sales[df_sales["Customer GSTN"] == gst_no]
 
-        sales_data = df_sales[df_sales["Customer GSTN"] == gst_no]
+                if not sales_data.empty:
+                    first_sale = sales_data["Invoice Date"].min()
+                    result.append(
+                        {
+                            "name": customer_name if pd.notna(customer_name) else "",
+                            "branch": branch if pd.notna(branch) else "",
+                            "person": sales_person if pd.notna(sales_person) else "",
+                            "gst": gst_no if pd.notna(gst_no) else "",
+                            "first_sale": (
+                                first_sale.strftime("%Y-%m-%d") if pd.notna(first_sale) else ""
+                            ),
+                        }
+                    )
 
-        if not sales_data.empty:
-            first_sale = sales_data["Invoice Date"].min()
-            result.append(
-                {
-                    "name": customer_name if pd.notna(customer_name) else "",
-                    "branch": branch if pd.notna(branch) else "",
-                    "person": sales_person if pd.notna(sales_person) else "",
-                    "gst": gst_no if pd.notna(gst_no) else "",
-                    "first_sale": (
-                        first_sale.strftime("%Y-%m-%d") if pd.notna(first_sale) else ""
-                    ),
-                }
+            # Cache the result for future use
+            cache.set(cache_key, result)
+            cache.set(cache_tmstmp_key, timezone.now())
+
+        except FileNotFoundError:
+            return JsonResponse(
+                {"error": "CSV file not found"},
+                status=406,
             )
 
     return result
